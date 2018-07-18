4cd2e227ccf4fd0cbf72e4e1a758e4059b1be096
Steffen Eichenberg <steffen.eichenberg@gmail.com>
2018-07-18 11:10:22 +0200

    add chat

--- client/shared/Chat.hs
+++ client/shared/Chat.hs
@@ -9,15 +9,13 @@
 {-# LANGUAGE TypeOperators #-}
 module Chat
   (
-    API
-  , Model
+    Model
   , Action
-  , Field
-  , Login.view
+  , Chat.view
 #ifdef __GHCJS__
-  , Login.update
+  , Chat.update
 #endif
-  , Login.initialModel
+  , Chat.initialModel
   ) where
 
 import Data.Proxy
@@ -30,19 +28,84 @@ import Servant.Client.Ghcjs
 import Servant.Client.Internal.XhrClient(runClientMOrigin)
 #endif
 
-import Businesstypes
+import qualified Businesstypes as BT
+import qualified ChatRooms as CRS
+import qualified ChatRoom as CR
 
 
 -- REST API
 
 -- MODELS
 
+data Model = Model
+    { participant :: BT.Participant
+    , chatRoomModel :: Maybe CR.Model
+    , chatRoomsModel :: CRS.Model
+    }
+
+
+initialModel :: BT.Participant -> Model
+initialModel participant = Model participant Nothing (CRS.initialModel)
+
+
 -- ACTIONS
+
+data Action
+    = HandleChatRoomsAction CRS.Action
+    | HandleChatRoomAction CR.Action
+
 
 -- VIEWS
 
+view :: Model -> View Action
+view model =
+    div_ [ class_ "row" ]
+         [ div_ [ class_ "col-md-6" ]
+            [ fmap HandleChatRoomsAction (CRS.view (chatRoomsModel model))
+            ]
+        , div_ [ class_ "col-md-6" ]
+            [ case (chatRoomModel model) of
+                Just crm ->
+                    fmap HandleChatRoomAction (CR.view crm)
+
+                Nothing ->
+                    div_ [] []
+            ]
+        ]
+
+
 #ifdef __GHCJS__
 -- UPDATE
+
+update :: Action -> Model -> Effect Action Model
+update msg model =
+    case msg of
+        HandleChatRoomsAction (CRS.Selected chatRoom) ->
+          (model {chatRoomModel = Just (CR.initialModel (participant model) chatRoom)}) <# do
+            return (HandleChatRoomAction CR.GetChatHistory)
+
+        HandleChatRoomsAction (CRS.Deselected) ->
+          noEff (model {chatRoomModel = Nothing})
+
+        HandleChatRoomsAction msg_ ->
+          let (Effect rm ra) = CRS.update msg_ (chatRoomsModel model)
+              newModel = model {chatRoomsModel = rm}
+              newAction = fmap (mapSub HandleChatRoomsAction) ra
+          in
+            Effect newModel newAction
+
+        HandleChatRoomAction msg_ ->
+          case (chatRoomModel model) of
+            Nothing ->
+              noEff model
+          
+            Just model_ ->
+              let (Effect rm ra) = CR.update msg_ model_
+                  newModel = model {chatRoomModel = Just rm}
+                  newAction = fmap (mapSub HandleChatRoomAction) ra
+              in
+                Effect newModel newAction
+
 
 -- REST-CLIENT
 #endif
